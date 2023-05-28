1e800a854ab63e95db85c65125d6512976bb8b5c
moktv2 <129290876+moktv2@users.noreply.github.com>
2023-05-28 16:52:38 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,16 +16,16 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
 global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
+global.ownername = "Mohammed_Gamer❤️" //ur owner name
+global.ytname = "YT: Mohammed_Gamer" //ur yt chanel name
 global.socialm = "GitHub: ELGAZAR" //ur github or insta name
-global.location = "Africa, Egypt, Cairo" //ur location
+global.location = "saudia" //ur location
 
 //bot bomdy 
 global.ownernomer = "201098906252" //ur number
 global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
+global.botname = '❤️Mohammed BOT❤️' //ur bot name
+global.linkz = "https://www.youtube.com/@mohammed_gamer9252" //your theme url which will be displayed on whatsapp
 global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
 global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
 global.themeemoji = "♥" //ur theme emoji
